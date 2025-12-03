66a8330f24b13966429cc04f6454e2eb5314ea5f
ggredilla <gaizka.gredilla@opendeusto.es>
2025-12-03 11:15:25 +0100

    Superman

--- WikiApp/views.py
+++ WikiApp/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import SuperHeroe  # 1. Importar el modelo
 
-# Create your views here.
+# La función index ahora carga los datos y renderiza una plantilla
 def index(request):
-    return HttpResponse('primera vista')
+    # 2. Recuperar todos los objetos SuperHeroe de la base de datos
+    lista_superheroes = SuperHeroe.objects.all()
+
+    # 3. Crear el contexto para enviar los datos a la plantilla
+    contexto = {
+        'superheroes': lista_superheroes,
+        'titulo_pagina': 'Lista de Superhéroes'
+    }
+
+    # 4. Renderizar la plantilla 'lista_superheroes.html'
+    return render(request, 'WikiApp/lista_superheroes.html', contexto)
