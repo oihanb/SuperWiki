a8854671483c4e0243c845fa497aaa5e43bd3d4e
ggredilla <gaizka.gredilla@opendeusto.es>
2025-12-06 13:06:33 +0100

    Grupo de SuperHeroes arreglado

--- WikiApp/urls.py
+++ WikiApp/urls.py
@@ -8,20 +8,20 @@ urlpatterns = [
 
     #LISTADO EDITORIALES
     #path('listaConfederaciones/', views.listaConfederacion,name='listaConfederacion'),
-    path('listaEditoriales/',views.listaEditorial.as_view(),name='listaEditorial'),
+    path('listaeditorial/',views.listaEditorial.as_view(),name='listaeditorial'),
     #DETALLE DE EDITORIALES
     #path('detalleConfederacion/<int:id_confederacion>/', views.detalleConfederacion,name='detalleConfederacion'),
-    path('detalleEditorial/<int:pk>', views.detalleEditorial.as_view(),name='detalleEditorial'),
+    path('detalleeditorial/<int:pk>', views.detalleEditorial.as_view(),name='detalleeditorial'),
     #LISTADO DE GRUPODESUPERHEROES
     #path('listaSelecciones/', views.listaSeleccion,name='listaSeleccion'),
-    path('listagrupodesuperheroes/', views.listaGrupoDeSuperHeroes.as_view(),name='listaGrupoDeSuperHeroes'),
+    path('listagrupodesuperheroes/', views.listaGrupoDeSuperHeroes.as_view(),name='listagrupodesuperheroes'),
     #DETALLE DE GRUPODESUPERHEROES
     #path('detalleSeleccion/<int:id_seleccion>/', views.detalleSeleccion,name='detalleSeleccion'),
-    path('detalleGrupoDeSuperHeroes/<int:pk>/', views.detalleGrupoDeSuperHeroes.as_view(),name='detalleGrupoDeSuperHeroes'),
+    path('detallegrupodesuperheroes/<int:pk>/', views.detalleGrupoDeSuperHeroes.as_view(),name='detallegrupodesuperheroes'),
     #LISTADO DE SUPERHEROE
     #path('listaFutbolistas/', views.listaFutbolista,name='listaFutbolista'),
-    path('listaSuperHeroes/', views.listaSuperHeroe.as_view(),name='listaSuperHeroes'),
+    path('listasuperheroes/', views.listaSuperHeroe.as_view(),name='listasuperheroes'),
     #DETALLE DE SUPERHEROES
     #path('detalleFutbolista/<int:id_futbolista>/', views.detalleFutbolista,name='detalleFutbolista')
-    path('detalleSuperHeroes/<int:pk>/', views.detalleSuperHeroe.as_view(),name='detalleSuperHeroe')
+    path('detallesuperheroes/<int:pk>/', views.detalleSuperHeroe.as_view(),name='detallesuperheroes')
 ]
