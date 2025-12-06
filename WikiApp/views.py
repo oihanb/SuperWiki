3bf102944de2b02b6a827337e79d69543a7a3356
oihanb <153312616+oihanb@users.noreply.github.com>
2025-12-06 19:58:31 +0100

    version fea pero funcional

--- WikiApp/views.py
+++ WikiApp/views.py
@@ -7,39 +7,39 @@ from django.views.generic import ListView,DetailView
 
 # Vistas de lista
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'WikiApp/index.html')
 
 
 #VISTAS EDITORIAL
 class detalleEditorial(DetailView):
     model=Editorial
-    template_name='detalleeditorial.html'
+    template_name='WikiApp/detalleeditorial.html'
     context_object_name='editorial'
 
 class listaEditorial(ListView):
     model= Editorial
-    template_name='listaeditorial.html'
+    template_name='WikiApp/listaeditorial.html'
     context_object_name='editorial_list'
 
 #VISTAS GRUPOSDESUPERHEROES
 class detalleGrupoDeSuperHeroes(DetailView):
     model=GrupoDeSuperHeroes
-    template_name='grupodesuperheroes.html'
+    template_name='WikiApp/detallegrupodesuperheroes.html'
     context_object_name='grupodesuperheroes'
 
 class listaGrupoDeSuperHeroes(ListView):
     model= GrupoDeSuperHeroes
-    template_name='listagrupodesuperheroes.html'
+    template_name='WikiApp/listagrupodesuperheroes.html'
     context_object_name='grupodesuperheroes_list'
 
 #VISTAS SUPERHEROES
 class detalleSuperHeroe(DetailView):
     model=SuperHeroe
-    template_name='detallesuperheroe.html'
+    template_name='WikiApp/detallesuperheroe.html'
     context_object_name='superheroe'
 
 class listaSuperHeroe(ListView):
     model= SuperHeroe
-    template_name='listasuperheroes.html'
+    template_name='WikiApp/listasuperheroes.html'
     context_object_name='superheroes_list'
     queryset=SuperHeroe.objects.order_by('nombre')
