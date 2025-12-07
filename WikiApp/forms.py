6ddce1e2c89f959d2c2ada53bcf0df2db1b6051e
oihanb <153312616+oihanb@users.noreply.github.com>
2025-12-07 20:28:26 +0100

    Icono de pestaña

--- WikiApp/forms.py
+++ WikiApp/forms.py
@@ -1,11 +1,14 @@
 from django import forms
 from .models import SuperHeroe
+from django.utils.translation import gettext_lazy as _
 
 class SuperHeroeForm(forms.ModelForm):
     class Meta:
         model = SuperHeroe
         fields = ['nombre', 'poderes', 'Fnacimiento', 'imagenSuperHeroe']
         labels = {
-            'nombre': 'Superhero Name',
-            'Fnacimiento': 'Año de Nacimiento (Año)',
+            'nombre': _('Name'),
+            'poderes': _('Powers'),
+            'Fnacimiento': _('Birth Year (Year)'),
+            'imagenSuperHeroe': _('Superhero Image'),
         }
