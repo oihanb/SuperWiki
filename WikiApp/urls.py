d6a8f012d190f9cf04d403266d9b9382fcdc41bd
ggredilla <gaizka.gredilla@opendeusto.es>
2025-12-07 18:29:27 +0100

    traducir formulario

--- WikiApp/urls.py
+++ WikiApp/urls.py
@@ -7,22 +7,17 @@ urlpatterns = [
     path('inicio',views.index,name='index'),
 
     #LISTADO EDITORIALES
-    #path('listaConfederaciones/', views.listaConfederacion,name='listaConfederacion'),
     path('listaeditorial/',views.listaEditorial.as_view(),name='listaeditorial'),
     #DETALLE DE EDITORIALES
-    #path('detalleConfederacion/<int:id_confederacion>/', views.detalleConfederacion,name='detalleConfederacion'),
     path('detalleeditorial/<int:pk>', views.detalleEditorial.as_view(),name='detalleeditorial'),
     #LISTADO DE GRUPODESUPERHEROES
-    #path('listaSelecciones/', views.listaSeleccion,name='listaSeleccion'),
     path('listagrupodesuperheroes/', views.listaGrupoDeSuperHeroes.as_view(),name='listagrupodesuperheroes'),
     #DETALLE DE GRUPODESUPERHEROES
-    #path('detalleSeleccion/<int:id_seleccion>/', views.detalleSeleccion,name='detalleSeleccion'),
     path('detallegrupodesuperheroes/<int:pk>/', views.detalleGrupoDeSuperHeroes.as_view(),name='detallegrupodesuperheroes'),
     #LISTADO DE SUPERHEROE
-    #path('listaFutbolistas/', views.listaFutbolista,name='listaFutbolista'),
     path('listasuperheroes/', views.listaSuperHeroe.as_view(),name='listasuperheroes'),
     #DETALLE DE SUPERHEROES
-    #path('detalleFutbolista/<int:id_futbolista>/', views.detalleFutbolista,name='detalleFutbolista')
     path('detallesuperheroes/<int:pk>/', views.detalleSuperHeroe.as_view(),name='detallesuperheroes'),
+    #CREAR SUPERHEROE
     path('crearsuperheroe/', views.SuperHeroeCreateView.as_view(), name='crearsuperheroe'),
 ]
